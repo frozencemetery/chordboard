828b5251c5d3f6268b24b4630757980ebd7bcb7f
Robbie Harwood <rharwood@pm.me>
2022-01-10 21:32:43 -0500

    Become interactive and define input format
    Signed-off-by: Robbie Harwood <rharwood@pm.me>

--- main.py
+++ main.py
@@ -1,31 +1,54 @@
 #!/usr/bin/python3
 # SPDX-License-Identifier: AGPL-3.0-only
 
-# Text format should look something like:
-#   X 0 1 2 3 4
-#   -----------
-# 3 | | * | | |
-#   | | | * | |
-#   | | | | * |
-#   | | | | | *
-#   | | | | | |
+import argparse
 
-fingerings = ['X', '0', '1', '2', '3', '4']
-offset = 3
-frets = [0, 0, 1, 2, 3, 4]
+def sl(fret: str, n: int) -> str:
+    if fret in ['x', 'X', '0']:
+        return fret
+    return str(int(fret) - n)
 
-print(f"  {' '.join(fingerings)}")
-print("  -----------")
-for i in range(5):
-    row = []
-    if i == 0 and offset != 0:
-        row.append(str(offset))
-    else:
-        row.append(' ')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--position", type=int, default=0)
+    parser.add_argument("frets")
+    parser.add_argument("fingers")
+    args = parser.parse_args()
 
+    fingers = args.fingers.split(",")
+    frets = args.frets.split(",")
+
+    minfret = -1
+    maxfret = 0
     for fret in frets:
-        if fret != 0 and i == fret:
-            row.append(" *")
+        if fret in ['x', 'X']:
+            continue
+        fret = int(fret)
+        if fret > maxfret:
+            maxfret = fret
+        elif minfret == -1 or fret < minfret:
+            minfret = fret
+
+    position = args.position
+    if position == 0:
+        position = max(min(minfret, maxfret - 5), 1)
+        if position > 1: # automatic position adjustment
+            frets = [sl(fret, position - 1) for fret in frets]
+            maxfret -= position - 1
+
+    pad = ' ' * len(str(position))
+    print(f"{pad} {' '.join(fingers)}")
+    print(f"{pad} -----------")
+    for i in range(1, max(maxfret, 4) + 1):
+        row = []
+        if i == 1 and position != 1:
+            row.append(str(position))
         else:
-            row.append(" |")
-    print(''.join(row))
+            row.append(pad)
+
+        for fret in frets:
+            if fret != 0 and str(i) == fret:
+                row.append(" *")
+            else:
+                row.append(" |")
+        print(''.join(row))
